426136a27ac5800c7038195105741f3337d497e7
Abid Hasan Muin <muin.739@gmail.com>
2025-03-17 14:41:49 +0600

    API is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {ApiService} from './services/api.service';
+import {NgForOf} from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: 'app-root', imports: [NgForOf], templateUrl: './app.component.html', styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-poc-app';
+  posts: any = [];
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.apiService.getPosts().subscribe((data) => {
+      this.posts = data;
+      console.log('API Response:', this.posts);
+    }, (error) => {
+      console.error('Error fetching data:', error);
+    });
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl = environment.apiBaseUrl;
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  getPosts() {
+    return this.httpClient.get(`${this.apiUrl}/posts`);
+  }
 }
